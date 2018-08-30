f0376c8d926aef9896dea91273ed7a3d012cfd20
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-08-30 12:07:29 -0500

    Commented up the code

--- gamerecord.py
+++ gamerecord.py
@@ -2,11 +2,24 @@ from prettytable import PrettyTable
 import helper
 
 class GameRecord:
+    """Records of all of the gaming relationships
+    
+    Contains all of the information surrounding storing the players, games, victories
+    and the relationships between them (friendships, wins, etc.). 
+
+    Attributes:
+        players: A dictionary formatted as {player_id : Player()} containing all of the players on record
+        games: A dictionary formatted as {game_id: Game()} containing all of the games on the record
+    """
+
     def __init__(self):
+        """Inits GameRecord with no players and no games"""
         self.players = {}
         self.games = {}
 
     def get_gamer_score(self, player):
+        """Fetches the gamerscore of a player given the player object"""
+
         total_score = 0
 
         for game_id in player.plays.keys():
@@ -17,12 +30,18 @@ class GameRecord:
     # These functions add items to the record and create new classes
 
     def add_player(self, player_id, player_name):
+        """Adds player to the players dictionary"""
+
         self.players[player_id] = helper.Player(player_id, player_name)
     
     def add_game(self, game_id, game_name):
+        """Adds game to the games dictionary"""
+
         self.games[game_id] = helper.Game(game_id, game_name)
     
     def add_victory(self, victory_id, game_id, victory_name, victory_points):
+        """Adds victory to the victory dictionary inside of the game object"""
+
         victory = helper.Victory(victory_id, game_id, victory_name, victory_points)
         self.games[game_id].add_victory(victory_id, victory)
 
@@ -31,14 +50,23 @@ class GameRecord:
     # These functions establish relationships and add to the classes information through functions
 
     def add_play(self, player_id, game_id, player_ign):
+        """Adds the game and gamertag to the plays dictionary while also 
+        adding the player_id to the list of players that play the game"""
+
         self.players[player_id].add_play(game_id, player_ign)
         self.games[game_id].add_player(player_id)
 
     def add_friends(self, player_id_1, player_id_2):
+        """Adds the player_id of each player to the other player's friends list"""
+
         self.players[player_id_1].add_friend(player_id_2)
         self.players[player_id_2].add_friend(player_id_1)
 
     def win_victory(self, player_id, game_id, victory_id):
+        """Adds the victory to the specified game to the player's victory dictionary, 
+        adds the player_id to the list of players who achieved the victory, and increasing
+        the number of times that victory was accomplished"""
+
         self.players[player_id].add_victory(game_id, victory_id)
         self.games[game_id].victories[victory_id].add_player(player_id)
         self.games[game_id].victories[victory_id].times_accomplished += 1
@@ -46,6 +74,8 @@ class GameRecord:
     #----------------------------------------------------------------------------------------
 
     def friends_who_play(self, player_id, game_id):
+        """Creates a report showing which player's friends play the game given with the game_id"""
+
         player = self.players[player_id]
         game = self.games[game_id]
 
@@ -56,6 +86,8 @@ class GameRecord:
 
         pt = PrettyTable()
         pt.field_names = ["Friend", "Gamerscore"]
+
+        #Checks if the friend in the list of friends has played the given game
         for friend_id in player.friends:
             if friend_id in game.self.players:
                 friend = self.players[friend_id]
@@ -63,7 +95,12 @@ class GameRecord:
         
         print(pt)
 
+    #TODO finish this function!!!
     def compare_players(self, player_id_1, player_id_2, game_id):
+        """Creates a report comparing player 1 and player 2's Victory 
+        records and total Victory scores for the given game"""
+
+        game = self.games[game_id]
         player_1 = self.players[player_id_1]
         player_2 = self.players[player_id_2]
 
@@ -71,33 +108,51 @@ class GameRecord:
         player_1_victory_ids = player_1.get_victories(game_id)
         player_2_victory_ids = player_2.get_victories(game_id)
 
-        for player_1_victory in player_1_victory_ids:
-            print (self.games[game_id].victories[player_1_victory]) #will update this to have actual output later
-        for player_2_victory in player_2_victory_ids:
-            print (self.games[game_id].victories[player_2_victory]) #will update this to have actual output later
+        print()
+        print("Game: " + game.name)
+        print()
+
+        #Player Comparison Table
+        pt = PrettyTable()
+        pt.field_names = ["Player", "Victory Record", "Total Points"]
+
+        victory_percentage = str(len(player_1_victory_ids)) + "/" + str(len(game.victories.keys())) 
+        pt.add_row([player_1.name, victory_percentage, player_1.get_score(game, game_id)])
+        victory_percentage = str(len(player_2_victory_ids)) + "/" + str(len(game.victories.keys()))
+        pt.add_row([player_2.name, victory_percentage, player_2.get_score(game, game_id)])
+
+        print(pt)
 
     def summarize_player(self, player_id):
+        """Creates a report of all of player's friends, games the
+         player plays, and gamer point totals."""
+
         player = self.players[player_id]
         print()
         print("Player: " + player.name)
         print("Total Gamescore: " + str(self.get_gamer_score(player)))
         print()
 
+        #Friend Table
         pt = PrettyTable()
         pt.field_names = ["Friend", "Gamerscore"]
+
         for friend_id in player.friends:
             friend = self.players[friend_id]
             pt.add_row([friend.name, self.get_gamer_score(friend)])
         print(pt)
         
+        #Games Table
         pt = PrettyTable()
         pt.field_names = ["Game", "Victories", "Gamerscore", "IGN"]
+
         for game_id in player.plays.keys():
             game = self.games[game_id]
             victory_percentage = str(len(player.get_victories(game_id))) + "/" + str(len(game.victories.keys()))
             pt.add_row([game.name, victory_percentage, player.get_score(game, game_id), player.plays[game_id]])
         print(pt)
         
+        #Getting the total game score
         total_game_score = 0
         for game_id in player.victories.keys():
             game = self.games[game_id]
@@ -105,8 +160,12 @@ class GameRecord:
             total_game_score += game_score
 
     def summarize_game(self, game_id):
+        """Creates a report of all players who play the specified game and the 
+        number of times each of its victories have been accomplished"""
+
         game = self.games[game_id]
 
+        #Player Table
         pt = PrettyTable()
         pt.field_names = ["Player", "Gamerscore", "IGN"]
         for player_id in game.players:
@@ -115,6 +174,7 @@ class GameRecord:
         
         print(pt)
 
+        #Victory Table
         pt = PrettyTable()
         pt.field_names = ["Victory", "Times Accomplished", "Points"]
         for victory_id in game.victories.keys():
@@ -125,14 +185,19 @@ class GameRecord:
             
 
     def summarize_victory(self, game_id, victory_id):
+        """Creates a report of all players who have achieved a Victory, and the 
+        percentage of players who play that game who have the Victory"""
+
         game = self.games[game_id]
 
         num_total_players = len(game.players)
         num_victories = game.victories[victory_id].times_accomplished
 
-
+        #Player Table
         pt = PrettyTable()
         pt.field_names = ["Player", "Gamerscore", "IGN"]
+
+        #Goes through players who play the game and checks if the player has achieved the victory given 
         for player_id in game.players:
             player = self.players[player_id]
             victories = player.get_victories(game_id)
@@ -140,17 +205,17 @@ class GameRecord:
                 pt.add_row([player.name, player.get_gamer_score(game, game_id), player.plays[game_id]])
         print(pt)
         
-        if num_total_players  == 0:
-            print (0)
-        else:
-            print(num_victories / num_total_players)
+        print ("Victory Achieved: " + num_victories + "/" + num_total_players)
 
     def victory_ranking(self):
+        """Creates a report showing the summary ranking all players by their total number of gamer points"""
+
         ranking = {}
 
         pt = PrettyTable()
         pt.field_names = ["Player", "Gamerscore"]
 
+        #Gets the gamerscore for each player and orders them based on highest to lowest gamerscore
         for player in self.players.values():
             total_game_score = 0
             for game_id in player.victories.keys():
@@ -160,6 +225,7 @@ class GameRecord:
 
             ranking[player.id] = total_game_score
         
+        #Returns a descending sorted list of tuples by gamerscore in the format (player_id, gamerscore)
         sorted_ranking = sorted(ranking.items(), key=lambda x:x[1], reverse=True)
 
         for rank in sorted_ranking:
--- helper.py
+++ helper.py
@@ -1,5 +1,23 @@
 class Player:
+    """A record of a player, the games they play, friends, etc.
+    
+    Contains all of the information pertinent to the player including the player_id,
+    player_name, games the player plays, friends, and their victories associated with
+    the games that they play
+
+    Attributes:
+        id: An int corresponding to the player id
+        name: A string containing the player name
+        plays: A dictionary in the format {game_id, gamer_tag} containing a mapping
+            between the gamer tag with the game the player plays with that tag
+        friends: A list of player ids containing the ids of the player's friends
+        victories: A dictionary in the format {game_id: [victory_id]} containing a mapping
+            between the game the player plays and the victories that they have achieved
+            in that game
+    """
+
     def __init__(self, id, name):
+        """Inits the id and name provided, and provides empty structures for plays, friends, and victories"""
         self.id = id
         self.name = name
         self.plays = {}
@@ -7,24 +25,37 @@ class Player:
         self.victories = {}
 
     def add_play(self, game_id, player_ign):
+        """Add play to the plays dictionary given the game id and gamer tag"""
         self.plays[game_id] = player_ign
     
     def add_friend(self, friend_id):
+        """Add friend to the list of friends"""
         self.friends.append(friend_id)
     
     def add_victory(self, game_id, victory_id):
+        """Adds victory to the victories dictionary given the game id 
+        and adds the victory id to the list of victories assigned to the game id. 
+        It creates a new list of victories if a victory has never been achieved in
+        the game before"""
+
         if game_id not in self.victories.keys():
             self.victories[game_id] = [victory_id]
         else:
             self.victories[game_id].append(victory_id)
 
     def get_victories(self, game_id):
+        """Returns the list of victories assigned to the game if that player
+        has achieved a victory in the game, otherwise return an empty list"""
+
         if game_id not in self.victories.keys():
             return []
         else:
             return self.victories[game_id]
     
     def get_score(self, game, game_id):
+        """Gets the total score of all the victories that the player has achieved
+        in that particular game"""
+
         game_score = 0
         for victory_id in self.get_victories(game_id):
             game_score += game.victories[victory_id].points
@@ -32,20 +63,52 @@ class Player:
         return game_score
 
 class Game:
+    """A record of a game, the game's name, the players who play the game, etc.
+    
+    Contains all of the information pertinent to the game including the game id, the game
+    name, the players who play the game, and the victories associated with the game
+
+    Attributes:
+        id: An int corresponding to the game id
+        name: A string containing the game name
+        players: A list of player ids containing all of the players who play the game
+        victories: A dictionary in the format {victory_id: Victory()} containing the mapping
+            between the victory id and the actual victory object
+    """
+
     def __init__(self, id, name):
+        """Inits the id and name provided and provides empty structures for players and victories""" 
         self.id = id
         self.name = name
         self.players = []
         self.victories = {}
     
     def add_player(self, player_id):
+        """Adds player id to the list of players who play the game"""
         self.players.append(player_id)
     
     def add_victory(self, victory_id, victory):
+        """Adds the victory to the dictionary of victories assigned to the game"""
         self.victories[victory_id] = victory
     
 class Victory:
+    """A record of a victory, the victory name, the game that that victory can be achieved in, etc.
+    
+    Contains all of the information pertinent to the game including the victory id, the game that the
+    victory can be achieved in, the victory name, the points assigned to that victory, the number of 
+    times the victory is accomplished, and the players who have achieved the victory
+
+    Attributes:
+        victory_id: An int corresponding to the victory id
+        game_id: An int corresponding to the game id
+        name: A string containing the victory name
+        points: An int corresponding to the points achieved for achieving the victory
+        times_accomplished: An int corresponding to the number of times that victory is accomplished
+        players: A list of player ids corresponding to the players that have achieved the victory
+    """
+
     def __init__(self, victory_id, game_id, name, points):
+        """Inits the victory id, game id, name, and points provided and inits times_accomplished with 0 and players with an empty list"""
         self.victory_id = victory_id
         self.game_id = game_id
         self.name = name
@@ -54,4 +117,5 @@ class Victory:
         self.players = []
     
     def add_player(self, player_id):
+        """Adds player to the list of the players who have achieved the victory"""
         self.players.append(player_id)
